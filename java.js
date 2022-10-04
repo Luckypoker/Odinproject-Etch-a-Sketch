1efb8ad5fce3cba110fcd37141d84157e55e5947
Artificient <geniusatwork543@gmail.com>
2022-10-04 13:04:24 -0400

    Added a window prompt for user input and reworked java accordingly

--- java.js
+++ java.js
@@ -6,10 +6,26 @@ container.appendChild(content)
 const display = document.createElement('div')
 display.classList.add("display")
 content.appendChild(display)
-//boxes
-var boxes = [];
-for (let i = 0; i < 16; i++) {
-    const box = document.createElement('li')
-    box.classList.add('box')
-    display.appendChild(box)
+//User Input
+function getBoxAmount () {
+    answer = window.prompt('Please input an amount')
+    return answer;
 }
+//boxes
+getBoxAmount()
+display.style.cssText = `grid-template-columns: repeat(${answer}, auto); grid-template-rows: repeat(${answer}, auto);`
+const box1 = document.createElement('div')
+for (let i = 0; i < answer; i++) {
+    for (let j = 0; j < answer; j++) {
+        const box1 = document.createElement('div')
+        box1.classList.add('box1')
+        display.appendChild(box1)
+        window.addEventListener('mouseover', (colorSwap) => {
+            box1.classList.add('playing')
+            colorSwap.stopPropagation()
+            console.log(colorSwap)
+        })
+    }
+}
+
+    
